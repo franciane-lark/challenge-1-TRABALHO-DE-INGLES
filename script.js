a5939e05dabb9682fd61e15e1521a69bfae160dd
Franciane Lark <franciane.lark019@gmail.com>
2022-07-01 19:41:47 -0400

    update movies

---
+++ script.js
@@ -0,0 +1,17 @@
+const comedy =[{
+    tiltle: "Ferris Bueller's Day Off ",
+    diretor: "John Hughes ",
+    year: "1986"
+}]
+
+const action =[{
+    tiltle: " Kingsman: The Golden Circle ",
+    diretor: "Matthew Vaughn ",
+    year: "2017"
+}]
+
+const fantasy =[{
+    tiltle: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe ",
+    diretor: "Andrew Adamson ",
+    year: "2005"
+}]
